5c61e95a8e5932311e1a027ec3e8f847642bc685
Lampros Fasoulas <lamprosfasoulas@gmail.com>
2025-02-24 01:43:05 +0200

    i have to find out how to renew cache

--- pkg/files/render.go
+++ pkg/files/render.go
@@ -7,6 +7,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/lamprosfasoulas/docs/pkg/cache"
 )
@@ -25,7 +26,7 @@ func RunBat(f, lang string) ([]byte, error) {
     return output,nil
 }
 func GetHTML(c []byte) []byte {
-    aha := exec.Command("aha","--black","-n","-t","Skonaki")
+    aha := exec.Command("aha","--black","-n","-l","-t","Skonaki")
     aha.Stdin = bytes.NewReader(c)
 
     var ahaOut bytes.Buffer
@@ -49,11 +50,7 @@ func GetContent(path []string) []byte{
     var lang string //used for bat syntax highlighting
     var content string 
     var showDir string
-    // Here we get the directories under data
-    cmd := exec.Command("ls","data")
-    if dirs, err := cmd.CombinedOutput(); err == nil{
-        DIRS = strings.Fields(string(dirs))
-    }
+
     if path[0] == "" {
         path[0] = "home"
     }
@@ -68,31 +65,44 @@ func GetContent(path []string) []byte{
         path[0] = "_" + path[0]
     }
     key := filepath.Join(path...)
-    if c, e := cache.GetCont(key); e == nil && c != nil{
-        log.Printf("Getting key: %v from Cache",key)
-        return c
-    }
     //start := time.Now()
-    for _,dir := range DIRS{
-        //filePath := filepath.Join(ROOT,strings.Split(dir,".")[1],filepath.Join(path...))
-        filePath := filepath.Join(ROOT,dir,filepath.Join(path...))
-        //if c, e := cache.GetCont(filePath); e == nil && c != nil{
-        //    fmt.Println("found in cache",filePath,e)
-        //    content += string(c)
-        //}else{
+    inner := func() {
+        // Here we get the directories under data
+        cmd := exec.Command("ls","data")
+        if dirs, err := cmd.CombinedOutput(); err == nil{
+            DIRS = strings.Fields(string(dirs))
+        }
+        wg := sync.WaitGroup{}
+        for _,dir := range DIRS{
+            wg.Add(1)
+            //filePath := filepath.Join(ROOT,strings.Split(dir,".")[1],filepath.Join(path...))
+            filePath := filepath.Join(ROOT,dir,filepath.Join(path...))
+            //if c, e := cache.GetCont(filePath); e == nil && c != nil{
+            //    fmt.Println("found in cache",filePath,e)
+            //    content += string(c)
+            //}else{
             if output, err := RunBat(filePath,lang); err == nil {
                 tempC := fmt.Sprintf("\n\033[33;1m%v:%v\033[0m\n",strings.Split(dir,".")[1],showDir)
                 tempC += string(output)
                 //log.Printf("Failed to set cache: %v",cache.SetCont(filePath,[]byte(tempC)))
                 content += tempC
             }
-        //}
+            //}
+            wg.Done()
     }
     //log.Printf("\nTime for loop %v\n ----------",time.Since(start))
     if content == ""{
-        return GetContent([]string{"404"})
+        GetContent([]string{"404"})
     }
     cache.SetCont(key, []byte(content))
+    }
+    if c, e := cache.GetCont(key); e == nil && c != nil{
+        log.Printf("Getting key: %v from Cache",key)
+        //go inner()
+        return c
+    }else{
+        inner()
+    }
     return []byte(content)
 }
 
